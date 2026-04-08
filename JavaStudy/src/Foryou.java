16dd2d788818a69337984157a7f85d2b6e4739ab
Linkshimcat <axzenone@gmail.com>
2026-04-08 14:19:10 +0900

    java- for

---
+++ JavaStudy/src/Foryou.java
@@ -0,0 +1,10 @@
+public class Foryou {
+    public static void main(String[] args) {
+
+        char s = 65;
+        for (int a = s; s<=122;  s++) { System.out.println(s);
+
+        }
+
+    }
+}
---
+++ JavaStudy/src/Javs0407.java
@@ -0,0 +1,11 @@
+public class Javs0407 {
+    public static void main(String[] args) {
+
+        char lf = 10;
+        System.out.println("Hello" + lf + "World!");
+        System.out.println("Hello\nWorld");
+
+
+
+    }
+}
